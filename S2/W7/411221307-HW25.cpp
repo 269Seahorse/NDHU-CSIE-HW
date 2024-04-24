158deb47b3905d233d14a7eebc6448b5bea60cc9
269Seahorse <51509383+269Seahorse@users.noreply.github.com>
2024-04-24 14:09:08 +0800

    Gave Q25 more normal variables

--- S2/W7/411221307-HW25.cpp
+++ S2/W7/411221307-HW25.cpp
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class damn_oop {
+class oop {
 private:
     vector<int> st, av;
     int f;
 
 public:
-    damn_oop(vector<int> _st, vector<int> _av, int _f): f(_f) {
+    oop(vector<int> _st, vector<int> _av, int _f): f(_f) {
         st.assign(_st.begin(), _st.end());
         av.assign(_av.begin(), _av.end());
         sort(st.begin(), st.end());
@@ -31,14 +31,13 @@ int main() {
         for(int i = 0; i < f; i++) {
             cin >> st[i] >> av[i];
         }
-        damn_oop i_just_got_minori_in_gacha(st, av, f);
+        oop func(st, av, f);
         // sort(st, st + f);
         // sort(av, av + f);
         // int ans = f / 2 - !(f & 1);
 		// cout << "(Street: " << st[ans] << ", Avenue: " << av[ans] << ")" << endl; 
-        i_just_got_minori_in_gacha.ans();
+        func.ans();
 	}
 }
 
 // so why use oop
-// yes I'm using weird variables
